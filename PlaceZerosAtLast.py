e6a450bcf6097d378fa62110c3dc72c69ce7d8cf
PattewarAjay <pattewarajay11@gmail.com>
2026-02-18 22:27:49 +0530

    zeros to last

---
+++ PlaceZerosAtLast.py
@@ -0,0 +1,12 @@
+nums=[2,3,0,9,5,0,4,0,0,1]
+n=len(nums)
+temp=[]
+for i in range(0,n):
+    if nums[i]!=0:
+        temp.append(nums[i])
+nz=len(temp)
+for i in range(0,nz):
+    nums[i]=temp[i]
+for i in range(nz,n):
+    nums[i]=0
+print(nums)
--- RotateArrayByKPlaces.py
+++ RotateArrayByKPlaces.py
@@ -1,8 +1,12 @@
 a=[2,3,4,5,5,6,7,8,9,10]
+def reverse(a,left,right):
+    while left<right:
+        a[left],a[right]=a[right],a[left]
+        left+=1
+        right-=1
 n=len(a)
 k=3
-rotation=k%n
-for _ in range(0,k):
-    e=a.pop()
-    a.insert(0,e)
+reverse(a,n-k,n-1)
+reverse(a,0,n-k-1)
+reverse(a,0,n-1)
 print(a)
